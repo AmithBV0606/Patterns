b527c47aeff516d87e3424f0285c242ac60fe62f
AmithBV0606 <amithrao0606@gmail.com>
2025-01-02 17:45:57 +0530

    Star pyramid.

---
+++ 07_Star_Pyramid/README.md
@@ -0,0 +1,12 @@
+# Pattern - 7: Star Pyramid
+
+### Problem Statement: Given an integer N, print the following pattern : 
+
+<img src="../assets/Pic-13.png">
+Here, N = 5
+
+### Examples :
+<img src="../assets/Pic-14.png">
+
+### Workout :
+<img src="../assets/Star-Pyramid.jpeg" />
---
+++ 07_Star_Pyramid/Solution.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        // Spaces
+        for (int j = 0; j < (n - i - 1); j++)
+        {
+            cout << "  ";
+        }
+        
+        // Stars
+          for (int j = 0; j < (2 * i + 1); j++)
+        {
+            cout << "* ";
+        }
+        
+        // Spaces
+        for (int j = 0; j < (n - i - 1); j++)
+        {
+            cout << "  ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
